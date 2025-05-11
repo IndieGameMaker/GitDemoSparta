7be512f5423e6393ca495e7edd62e6a0c792d35b
Lee JaeHyun <myleje@gmail.com>
2025-05-11 22:46:03 +0900

    feat: Move Logic

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -13,6 +13,6 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.forward * Time.deltaTime * 2.0f;
     }
 }
