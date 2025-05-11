0357c3ed166c11cce2e12db07c603e15631ef095
Lee JaeHyun <myleje@gmail.com>
2025-05-12 07:58:52 +0900

    Update PlayerController.cs

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        Debug.Log("aaa");
     }
 
     // Update is called once per frame
